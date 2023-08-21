be42430087df583801372bb04ab3ad869e027042
Jeferson Luis Moraes Gonçalves <jgjefersonluis@gmail.com>
2023-08-21 11:05:14 -0300

    Task 01 - criar estrutura de pastas, mostrar a primeira janela, tratar eventos e inserir objetos na tela.

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("start no projeto")
+import pygame
+
+# tela tamanho
+display = pygame.display.set_mode((1280, 720))
+
+# variaveis
+player1 = pygame.Rect(0, 0, 30, 150)
+player2 = pygame.Rect(1250, 0, 30, 150)
+ball = pygame.Rect(600, 350, 15, 15)
+
+# lógica de repetição e decisão
+loop = True
+while loop:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            loop = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                loop = False
+
+    # mostra na tela
+    display.fill((0,0,0))
+    pygame.draw.rect(display, "white", player1)
+    pygame.draw.rect(display, "white", player2)
+    pygame.draw.circle(display, "white", ball.center, 8)
+    pygame.display.flip()
