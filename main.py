ef244ca39126ae18b4fad019eb3fd01224519847
Jeferson Luis Moraes Gonçalves <jgjefersonluis@gmail.com>
2023-08-23 23:39:40 -0300

    task 03 - textos e placar, textos indicadores e alteração no placar numerico

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame
-pygame.init()
 
+pygame.init()
+pygame.font.init()
 # tela tamanho
 display = pygame.display.set_mode((1280, 720))
-
 # variaveis
 player1 = pygame.Rect(0, 0, 30, 150)
+player1_score = 0
 player1_speed = 1
 
 player2 = pygame.Rect(1250, 0, 30, 150)
+player2_score = 0
 
 ball = pygame.Rect(600, 350, 15, 15)
 ball_dir_x = 1
 ball_dir_y = 1
 
+font = pygame.font.Font(None, 50)
+placar_player1 = font.render(str(player1_score), True, "white")
+placar_player2 = font.render(str(player2_score), True, "white")
 
 # lógica de repetição e decisão
 loop = True
@@ -25,10 +30,9 @@ while loop:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                player1_speed = 1
+                player1_speed = -1
             elif event.key == pygame.K_s:
                 player1_speed = 1
-
     # colisões
     if ball.colliderect(player1) or ball.colliderect(player2):
         ball_dir_x *= -1
@@ -43,9 +47,13 @@ while loop:
     player1.y += player1_speed
 
     if ball.x <= 0:
+        player2_score += 1
+        placar_player2 = font.render(str(player2_score), True, "white")
         ball.x = 600
         ball_dir_x *= -1
     elif ball.x >= 1280:
+        player1_score += 1
+        placar_player1 = font.render(str(player1_score), True, "white")
         ball.x = 600
         ball_dir_x *= -1
 
@@ -69,4 +77,6 @@ while loop:
     pygame.draw.rect(display, "white", player1)
     pygame.draw.rect(display, "white", player2)
     pygame.draw.circle(display, "white", ball.center, 8)
+    display.blit(placar_player1, (500, 50))
+    display.blit(placar_player2, (780, 50))
     pygame.display.flip()
